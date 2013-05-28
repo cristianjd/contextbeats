690ea90317859d572241213635f833e82223f97a
cristianjd <cristian.j.derr@gmail.com>
2013-05-28 00:57:53 -0700

    model validation, cleaned up appearance, styling of comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
+
+  validates :commenter, :presence => true
+  validates :body, :presence => true
+
+  #default_scope order: 'comments.created_at DESC'
 end
