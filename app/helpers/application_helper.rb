5f6901164c621647e31734fc767ae3781e1ece1e
cristianjd <cristian.j.derr@gmail.com>
2013-10-19 17:31:04 -0700

    display helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+
+  def formatted_time(item)
+    item.created_at.strftime("%B %d %Y, %I:%M %p")
+  end
 end
--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,6 @@
 module CommentsHelper
+
+  def comment_display(comment)
+    raw("<span class='comment_name'>#{comment.commenter}</span> said: \"<span class='comment_text'>#{comment.body}</span>\" on <span class='comment_date'>#{formatted_time(comment)}</span>.")
+  end
 end
